71e986675504f47e2dcbd2b58713bede1ac91ae7
Ilya Alesker <ilya.alesker@gmail.com>
2025-09-12 22:56:34 +0300

    Modularized plugins setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,6 +11,10 @@ end)
 
 vim.o.expandtab = true
 vim.o.breakindent = true
+vim.o.smartindent = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
 
 vim.o.undofile = true
 
@@ -85,10 +89,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- [[ lazy.nvim ]]
+-- [[ Plugins ]]
 --
 
--- Install
+-- lazy.nvim setup
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -106,109 +110,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
--- Setup
-
 require('lazy').setup({
   spec = {
-    -- import your plugins
-    -- { import = 'plugins' },
-    {
-      'ellisonleao/gruvbox.nvim',
-      priority = 1000,
-      config = true,
-      config = function()
-        require('gruvbox').setup {
-        }
-
-        vim.cmd.colorscheme 'gruvbox'
-      end,
-    },
-    {'NMAC427/guess-indent.nvim'},
-    {
-      'lewis6991/gitsigns.nvim',
-      opts = {
-        signs = {
-          add = { text = '+' },
-          change = { text = '~' },
-          delete = { text = '_' },
-          topdelete = { text = '‾' },
-          changedelete = { text = '~' },
-        },
-      },
-    },
-    {
-      'folke/which-key.nvim',
-      event = 'VimEnter',
-      opts = {
-        delay = 0,
-        spec = {
-          { '<leader>s', group = '[S]earch' },
-          { '<leader>t', group = '[T]oggle' },
-          { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-        },
-      },
-    },
-
-    {
-      'nvim-telescope/telescope.nvim',
-      event = 'VimEnter',
-      dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-telescope/telescope-ui-select.nvim',
-        'nvim-tree/nvim-web-devicons',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-      },
-      config = function()
-        require('telescope').setup {
-          extensions = {
-            ['ui-select'] = {
-              require('telescope.themes').get_dropdown(),
-            },
-          },
-          pickers = {
-            find_files = { hidden = true }
-          }
-        }
-
-        pcall(require('telescope').load_extension, 'fzf')
-        pcall(require('telescope').load_extension, 'ui-select')
-
-        local builtin = require 'telescope.builtin'
-        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-        vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-        vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-        vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-        vim.keymap.set('n', '<leader>/', function()
-          builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-            winblend = 10,
-            previewer = false,
-          })
-        end, { desc = '[/] Fuzzily search in current buffer' })
-
-        vim.keymap.set('n', '<leader>s/', function()
-          builtin.live_grep {
-            grep_open_files = true,
-            prompt_title = 'Live Grep in Open Files',
-          }
-        end, { desc = '[S]earch [/] in Open Files' })
-
-        vim.keymap.set('n', '<leader>sn', function()
-          builtin.find_files { cwd = vim.fn.stdpath 'config' }
-        end, { desc = '[S]earch [N]eovim files' })
-      end,
-    },
+    { import = 'plugins' },
   },
-  -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { 'desert' } },
-  -- automatically check for plugin updates
   checker = { enabled = true },
 })
 
