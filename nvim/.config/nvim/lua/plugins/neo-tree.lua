a4d4a39fc4a36eba2010d15dc2514b7e10fe6ea5
Ilya Alesker <ilya.alesker@gmail.com>
2025-09-12 22:57:44 +0300

    Added Neotree and Lualine

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, 
+        hide_dotfiles = true,
+        hide_gitignored = true,
+      },
+    }
+  },
+  config = function()
+    vim.keymap.set('n', '-', ':Neotree<CR>')
+  end
+}
