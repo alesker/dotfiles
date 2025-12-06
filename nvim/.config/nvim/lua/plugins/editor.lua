e335bebd2c4fe339564361674331d63fc6a67cb5
Ilya Alesker <ilya.alesker@gmail.com>
2025-12-06 19:21:29 +0200

    Added flash plugin

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -23,6 +23,30 @@ return {
     end,
   },
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    vscode = true,
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+    },
+  },
+  {
     "folke/which-key.nvim",
     event = "VimEnter",
     opts = {
