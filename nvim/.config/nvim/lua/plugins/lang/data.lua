34303968b2c467df7d53e74aac3575588ae6cf6a
Ilya Alesker <ilya.alesker@gmail.com>
2026-01-18 17:15:29 +0200

    Added tombi back

--- nvim/.config/nvim/lua/plugins/lang/data.lua
+++ nvim/.config/nvim/lua/plugins/lang/data.lua
@@ -18,20 +18,22 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      ensure_installed = { "jq", "yamlfmt" },
+      ensure_installed = { "jq", "yamlfmt", "tombi" },
       formatters_by_ft = {
         json = { "jq" },
         yaml = { "yamlfmt" },
+        toml = { "tombi" },
       },
     },
   },
   {
     "mfussenegger/nvim-lint",
     opts = {
-      ensure_installed = { "jq", "yamllint" },
+      ensure_installed = { "jq", "yamllint", "tombi" },
       linters_by_ft = {
         json = { "jq" },
         yaml = { "yamllint" },
+        toml = { "tombi" },
       },
       linters = {},
     },
