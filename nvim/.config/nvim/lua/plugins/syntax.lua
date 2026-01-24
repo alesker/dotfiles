400b365f653cc697c37837673648283da9f09f7c
Ilya Alesker <ilya.alesker@gmail.com>
2026-01-25 00:11:01 +0200

    Fixed treesitter textobjects config

--- nvim/.config/nvim/lua/plugins/syntax.lua
+++ nvim/.config/nvim/lua/plugins/syntax.lua
@@ -42,6 +42,49 @@ return {
     "nvim-treesitter/nvim-treesitter-textobjects",
     branch = "main",
     event = "VeryLazy",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      move = {
+        set_jumps = true,
+        keys = {
+          goto_next_start = {
+            ["]f"] = { query = "@function.outer", desc = "Next function start" },
+            ["]c"] = { query = "@class.outer", desc = "Next class start" },
+            ["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
+          },
+          goto_next_end = {
+            ["]F"] = { query = "@function.outer", desc = "Next function end" },
+            ["]C"] = { query = "@class.outer", desc = "Next class end" },
+            ["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
+          },
+          goto_previous_start = {
+            ["[f"] = { query = "@function.outer", desc = "Previous function start" },
+            ["[c"] = { query = "@class.outer", desc = "Previous class start" },
+            ["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
+          },
+          goto_previous_end = {
+            ["[F"] = { query = "@function.outer", desc = "Previous function end" },
+            ["[C"] = { query = "@class.outer", desc = "Previous class end" },
+            ["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      local treesitter = require("nvim-treesitter-textobjects")
+      treesitter.setup(opts)
+
+      for method, keymaps in pairs(opts.move.keys) do
+        for key, mapping in pairs(keymaps) do
+          vim.keymap.set({ "n", "x", "o" }, key, function()
+            require("nvim-treesitter-textobjects.move")[method](mapping.query, "textobjects")
+          end, {
+            desc = mapping.desc,
+            silent = true,
+          })
+        end
+      end
+    end,
   },
   {
     "norcalli/nvim-colorizer.lua",
