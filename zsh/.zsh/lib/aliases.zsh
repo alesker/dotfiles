4ed6c8fcc7776716a3518f31751557022e54e6ad
Ilya Alesker <ilya.alesker@gmail.com>
2026-04-28 17:56:31 +0300

    Replaced ls with eza

--- zsh/.zsh/lib/aliases.zsh
+++ zsh/.zsh/lib/aliases.zsh
@@ -13,6 +13,14 @@ alias ll='ls -lh'
 alias ls='ls -G'
 alias lsa='ls -lah'
 
+alias eza='eza --group-directories-first'
+alias ls='eza'
+alias ll='eza -lh'
+alias la='eza -lAh --git'
+alias lt='eza --tree --level=2 -A --git-ignore'
+alias ltt='eza --tree --level=3 -A --git-ignore'
+alias lttt='eza --tree --level=4 -A --git-ignore'
+
 alias cat='bat'
 alias vim='nvim'
 
